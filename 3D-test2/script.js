c4dec6cd593248018018cb2983332be47fd54291
irekanmi <cokerayanfe@gmail.com>
2022-03-21 13:57:57 +0100

    3D-test2

--- 3D-test2/script.js
+++ 3D-test2/script.js
@@ -4,17 +4,27 @@ const ctx = canvas.getContext("2d");
 canvas.width = 900;
 canvas.height = 600;
 
+
+// -------------FUNTIONS-----------------
+
+function animate() {
+  ftheta += 0.012;
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+  Engine.INIT();
+  requestAnimationFrame(animate);
+}
+
+
 // ----------------------MATH----------------
 
 // Vectors
 
 class Vec3d {
   
-  constructor(x,y,z,w = 1){
+  constructor(x,y,z){
     this.x = x;
     this.y = y;
     this.z = z;
-    this.w = w;
   }
 
   static Vector_Mul(vecA,vecB){
@@ -25,11 +35,11 @@ class Vec3d {
     return val;
   }
 
-  static Vector_Div(vecA,vecB){
+  static Vector_Div(vecA,num){
     let val = new Vec3d();
-    val.x = vecA.x / vecB.x;
-    val.y = vecA.y / vecB.y;
-    val.z = vecA.z / vecB.z;
+    val.x = vecA.x / num;
+    val.y = vecA.y / num;
+    val.z = vecA.z / num;
     return val;
   }
 
@@ -89,8 +99,8 @@ class Matrix {
 
   static Matrix_Mul(matA,matB){
     let val = new Matrix();
-    for(let c =0;c<3;c++){
-      for(let r = 0;r<3;r++){
+    for(let r =0;r<4;r++){
+      for(let c = 0;c<4;c++){
         val.m[r][c] = matA.m[r][0] * matB.m[0][c] + matA.m[r][1] * matB.m[1][c] + matA.m[r][2] * matB.m[2][c] + matA.m[r][3] * matB.m[3][c];
       }
     }
@@ -108,10 +118,18 @@ class Matrix {
 
   static Matrix_Mul_Vec(matrix,vec){
     let val = new Vec3d();
-    val.x = matrix.m[0][0] * vec.x + matrix.m[1][0] * vec.y + matrix.m[2][0] * vec.z + matrix.m[3][0] * vec.w;
-    val.y = matrix.m[0][1] * vec.x + matrix.m[1][1] * vec.y + matrix.m[2][1] * vec.z + matrix.m[3][1] * vec.w;
-    val.z = matrix.m[0][2] * vec.x + matrix.m[1][2] * vec.y + matrix.m[2][2] * vec.z + matrix.m[3][2] * vec.w;
-    val.w = matrix.m[0][3] * vec.x + matrix.m[1][3] * vec.y + matrix.m[2][3] * vec.z + matrix.m[3][3] * vec.w;
+    val.x = matrix.m[0][0] * vec.x + matrix.m[1][0] * vec.y + matrix.m[2][0] * vec.z + matrix.m[3][0];
+    val.y = matrix.m[0][1] * vec.x + matrix.m[1][1] * vec.y + matrix.m[2][1] * vec.z + matrix.m[3][1];
+    val.z = matrix.m[0][2] * vec.x + matrix.m[1][2] * vec.y + matrix.m[2][2] * vec.z + matrix.m[3][2];
+    
+    let w =  matrix.m[0][3] * vec.x + matrix.m[1][3] * vec.y + matrix.m[2][3] * vec.z + matrix.m[3][3];
+
+    if(  w != 0){
+      val.x /= w;
+      val.y /= w;
+      val.z /= w;
+    }
+
     return val;
   }
 
@@ -154,8 +172,8 @@ class Matrix {
     val.m[1][1] = 1;
     val.m[2][2] = 1;
     val.m[3][3] = 1;
-    val.m[3][0] = x;
-    val.m[3][1] = y;
+    val.m[0][3] = x;
+    val.m[1][3] = y;
     val.m[3][2] = z;
     return val;
   }
@@ -167,7 +185,7 @@ class Matrix {
     val.m[1][1] = fFOvRad;
     val.m[2][2] = fFar / (fFar - fNear);
     val.m[2][3] = 1;
-    val.m[3][2] = -((fFar * fNear ) / fFar - fNear);
+    val.m[3][2] = -(fFar * fNear ) / (fFar - fNear);
     return val;
   }
 
@@ -203,9 +221,15 @@ class Mesh {
 
 
 class Engine {
-  constructor(){};
+  constructor(){
+    this.Vcamera = new Vec3d(0,0,0);
+    this.lightDir = new Vec3d(0,0,-1);
+    this.fillStyle = "black";
+    this.strokeStyle = "black";
+  };
 
-  #DrawTriangles(a,b,c){
+  static DrawTriangles(a,b,c){
+  ctx.strokeStyle = this.strokeStyle;
   ctx.lineWidth = 1;
   ctx.beginPath();
   ctx.moveTo(a.x, a.y);
@@ -216,7 +240,8 @@ class Engine {
   ctx.stroke();
   }
 
-  #FillTriangles(a,b,c){
+  static FillTriangles(a,b,c){
+  ctx.fillStyle = this.fillStyle;
   ctx.lineWidth = 1;
   ctx.beginPath();
   ctx.moveTo(a.x, a.y);
@@ -227,7 +252,181 @@ class Engine {
   ctx.fill();
   }
 
+  static INIT(){
+    let matWorld = Matrix.Make_Identity();
+    let matWorld2 ;
+    let matRotZ = Matrix.Matrix_RotateZ(ftheta * 0.4);
+    let matRotX = Matrix.Matrix_RotateX(ftheta * 0.6);
+    let matProj = Matrix.Make_Projection(90,(canvas.height / canvas.width),1000,0.1);
+    let matTrans = Matrix.Matrix_Translation(0,0,4);
+
+    matWorld = Matrix.Matrix_Mul(matRotZ,matRotX);
+    matWorld2 = Matrix.Matrix_Mul(matWorld,matTrans);
+    for(let i = 0;i < BoxMesh.m.length;i++){
+      let triTransformed = new Triangle();
+      let triProjected = new Triangle();
+      triTransformed.p[0] = Matrix.Matrix_Mul_Vec(matWorld2,BoxMesh.m[i].p[0]);
+      triTransformed.p[1] = Matrix.Matrix_Mul_Vec(matWorld2,BoxMesh.m[i].p[1]);
+      triTransformed.p[2] = Matrix.Matrix_Mul_Vec(matWorld2,BoxMesh.m[i].p[2]);
+
+
+      let normal, line1, line2;
+      line1 = Vec3d.Vector_Sub(triTransformed.p[1],triTransformed.p[0]);
+      line2 = Vec3d.Vector_Sub(triTransformed.p[2],triTransformed.p[0]);
+
+      normal = Vec3d.Vector_Cross(line1,line2);
+      normal = Vec3d.Vector_Normalize(normal);
+      let Vcamera = new Vec3d(0,0,-1);
+
+      // Vcamera = Vec3d.Vector_Sub(triTransformed.p[0],Vcamera);
+
+      let viewdp = Vec3d.Vector_Dp(normal,Vcamera);
+
+      if( viewdp > 0 ){
+        let lightDir = new Vec3d(0,0,-1);
+        lightDir = Vec3d.Vector_Normalize(lightDir);
+
+        let lightdp = Math.max(0.03,Vec3d.Vector_Dp(lightDir,normal));
+        let red = 95 * lightdp;
+        let blue = 132 * lightdp;
+        let green = 120 * lightdp;
+        this.fillStyle = `rgb(${red},${green},${blue})`
+        this.strokeStyle = `rgb(${red},${green},${blue})`
+
+        triProjected.p[0] = Matrix.Matrix_Mul_Vec(matProj,triTransformed.p[0]);
+        triProjected.p[1] = Matrix.Matrix_Mul_Vec(matProj,triTransformed.p[1]);
+        triProjected.p[2] = Matrix.Matrix_Mul_Vec(matProj,triTransformed.p[2]);
+
+        let vOffset = new Vec3d(1,1,0);
+        triProjected.p[0] = Vec3d.Vector_Add(triProjected.p[0],vOffset);
+        triProjected.p[1] = Vec3d.Vector_Add(triProjected.p[1],vOffset);
+        triProjected.p[2] = Vec3d.Vector_Add(triProjected.p[2],vOffset);
+
+      triProjected.p[0].x *= (0.5 * (canvas.width));
+      triProjected.p[0].y *= (0.5 * (canvas.height));
+      triProjected.p[1].x *= (0.5 * (canvas.width));
+      triProjected.p[1].y *= (0.5 * (canvas.height));
+      triProjected.p[2].x *= (0.5 * (canvas.width));
+      triProjected.p[2].y *= (0.5 * (canvas.height));
+
+      this.DrawTriangles(triProjected.p[0],triProjected.p[1],triProjected.p[2]);
+      this.FillTriangles(triProjected.p[0],triProjected.p[1],triProjected.p[2]);
+
+
+      }
+
+    }
+
+  }
+
 }
 
 
-// -------------------------------------------------------INITIALIZATION-----------------------
+// -------------------------------------------------------INITIALIZATION--------------------------
+
+const box = [
+
+    // SOUTH
+    new Triangle(
+      new Vec3d(0,0,0),
+      new Vec3d(0,1,0),
+      new Vec3d(1,1,0)
+    ),
+    new Triangle(
+      new Vec3d(0,0,0),
+      new Vec3d(1,1,0),
+      new Vec3d(1,0,0)
+    ),
+  
+    // NORTH
+    new Triangle(
+      new Vec3d(1,0,1),
+      new Vec3d(1,1,1),
+      new Vec3d(0,1,1)
+    ),
+    new Triangle(
+      new Vec3d(1,0,1),
+      new Vec3d(0,1,1),
+      new Vec3d(0,0,1)
+    ),
+  
+    // TOP
+    new Triangle(
+      new Vec3d(0,1,0),
+      new Vec3d(0,1,1),
+      new Vec3d(1,1,1)
+    ),
+    new Triangle(
+      new Vec3d(0,1,0),
+      new Vec3d(1,1,1),
+      new Vec3d(1,1,0)
+    ),
+  
+    // BOTTOM
+    new Triangle(
+      new Vec3d(1,0,1),
+      new Vec3d(0,0,1),
+      new Vec3d(0,0,0)
+    ),
+    new Triangle(
+      new Vec3d(1,0,1),
+      new Vec3d(0,0,0),
+      new Vec3d(1,0,0)
+    ),
+  
+    // EAST
+    new Triangle(
+      new Vec3d(1,0,0),
+      new Vec3d(1,1,0),
+      new Vec3d(1,1,1)
+    ),
+    new Triangle(
+      new Vec3d(1,0,0),
+      new Vec3d(1,1,1),
+      new Vec3d(1,0,1)
+    ),
+  
+    // WEST
+    new Triangle(
+      new Vec3d(0,0,1),
+      new Vec3d(0,1,1),
+      new Vec3d(0,1,0)
+    ),
+    new Triangle(
+      new Vec3d(0,0,1),
+      new Vec3d(0,1,0),
+      new Vec3d(0,0,0)
+    )
+
+
+];
+const BoxMesh = new Mesh(box);
+let ftheta = 0;
+
+animate();
+// let matA = new Matrix();
+
+// matA.m[0][0] = 1;
+// matA.m[0][1] = 2
+// matA.m[0][2] = 3
+// matA.m[0][3] = 4
+// matA.m[1][0] = 5
+// matA.m[1][1] = 6
+// matA.m[1][2] = 7
+// matA.m[1][3] = 8
+
+// console.log(matA);
+// let matB = new Matrix();
+
+// matB.m[0][0] = 1;
+// matB.m[0][1] = 2
+// matB.m[0][2] = 3
+// matB.m[0][3] = 4
+// matB.m[1][0] = 5
+// matB.m[1][1] = 6
+// matB.m[1][2] = 7
+// matB.m[1][3] = 8
+
+// console.log(matB);
+
+// console.log(Matrix.Matrix_Mul(matA,matB));
